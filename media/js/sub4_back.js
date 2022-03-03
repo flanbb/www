63ef79a1e2130602432718f9af6a76622178201f
jjw <flannels@naver.com>
2022-03-03 23:41:50 +0900

    반응형 웹 프로젝트 추가

---
+++ media/js/sub4_back.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    var screenSize = $(window).width();
+    var screenHeight = $(window).height();
+
+    $("#content").css('margin-top',screenHeight);
+
+    if( screenSize > 768){
+        $('.videoBox>img').attr('src','images/sub4_bg1.png');
+    }
+    if(screenSize <= 768){
+        $('.videoBox>img').attr('src','images/sub4_bg2.png');
+    }
+    
+    $(window).resize(function(){   
+    screenSize = $(window).width(); 
+    screenHeight = $(window).height();
+        
+    $("#content").css('margin-top',screenHeight);
+        
+    if( screenSize > 768){
+        $('.videoBox>img').attr('src','images/sub4_bg1.png');
+    }
+    if(screenSize <= 768){
+        $('.videoBox>img').attr('src','images/sub4_bg2.png');
+    }
+    }); 
+
+   $('.down').click(function(){
+		screenHeight = $(window).height();
+		$('html,body').animate({'scrollTop':screenHeight}, 1000);
+	});
+
+});
